dcacd3eee366d074846a134680d2a0ed5f1a1e67
MayaTavares <tavares.pereira@aluno.ifsp.edu.br>
2022-06-01 08:30:38 -0300

    first commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// somar 1,2 + 3,4
+
+var operacao = args[0];
+var n1 = Convert.ToDouble(args[1]);
+var n2 = Convert.ToDouble(args[2]);
+
+// dotnet run -- ['Somar' '1,2' '3,4']
+
+if (operacao == "Somar")
+{
+    Console.WriteLine(n1 + n2);
+}
